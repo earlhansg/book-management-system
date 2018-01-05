82aad1cf5f2b6c6a6e46c514a4279f4e3699b3ed
earlhansg <earlhansg@gmail.com>
2018-01-06 02:15:28 +0800

    #15 API for Adding Users

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,23 @@ router.get('/', function(req, res, next) {
   .then(data => res.json(data));
 });
 
+// POST new user
+router.post('/', function (req, res){
+  const created_at = new Date();
+  db.
+  users
+  .create({
+    username: req.body.username,
+    password: req.body.password,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    middlename: req.body.middlename,
+    email: req.body.email,
+    created_at: created_at
+  })
+  .then(data => {
+    res.json(data);
+  });
+});
 
 module.exports = router;
--- server/models/users.js
+++ server/models/users.js
@@ -3,16 +3,36 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     id: {
-      type: DataTypes.UUID,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: 6
+                }
+            }
     },
     firstname: {
       type: DataTypes.STRING,
       required: true
     },
     lastname: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    middlename: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    email: {
       type: DataTypes.STRING,
       required: true
     },
