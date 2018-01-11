8a7b9f29007e526c411d54f7340fd49325e910bd
earlhansg <earlhansg@gmail.com>
2018-01-12 01:16:10 +0800

    #21 API Authentication for Login

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,75 @@ var jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt-nodejs');
 
 
+router.post('/authenticate', function(req, res) {
+  db
+  .users
+  .findOne(
+    {
+      where:
+        {  username: req.body.username }
+    }
+  ).then(user => {
+    // console.log(user);
+    if(!user) {
+      res.send({message: "Authentication failed. User not found.", success:false});
+    }
+    else {
+      if(bcrypt.compareSync(req.body.password, user.password)) {
+        var myToken = jwt.sign({ user: user.id },
+                                         'secret',
+                                        { expiresIn: 24 * 60 * 60 });
+        res.json(200, { token: myToken,
+                        userId: user.id,
+                        username: user.username,
+                        firstname: user.firstname,
+                        lastname: user.lastname,
+                        middlename: user.middlename,
+                        email: user.email
+                        });
+      } else {
+        res.send({message: "Invalid password", success: false });
+      }
+    }
+
+  })
+});
+
+router.use(function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.params.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token)
+    {
+
+        // verifies secret and checks exp
+        jwt.verify(token, 'secret', function(err, decoded) {
+          if (err) {
+             console.error('JWT Verification Error', err);
+             return res.status(403).send(err);
+          } else {
+             req.decoded = decoded;
+             return next();
+          }
+        });
+
+    }
+    else
+    {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   return db
@@ -33,32 +102,5 @@ router.post('/', function (req, res){
   });
 
 });
-
-// router.post('/authenticate', function(req, res) {
-//   db
-//   .users
-//   .findOne(
-//     {
-//       username: req.body.username
-//     }
-//   ).select('password').exec(function(err, user){
-//     if (err) throw err;
-//     if(!user){
-//        res.send({message: "Authentication failed. User not found.", success:false});
-//     }
-//     else if (user) {
-//
-//     }
-//   })
-//   .then(user => {
-//     var myToken = jwt.sign({ user: user.id },
-//                                      'secret',
-//                                     { expiresIn: 24 * 60 * 60 });
-//     res.send(200, {'token': myToken,
-//                    'userId': user.id,
-//                    'username': user.username });
-//
-//   })
-// });
 
 module.exports = router;
