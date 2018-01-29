8d73bef680e1f364d664e71d393033599c57c5dd
earlhansg <earlhansg@gmail.com>
2018-01-30 01:17:41 +0800

    #32 User Service Setup

---
+++ public/app/src/app/dashboard/user/user.model.ts
@@ -0,0 +1,8 @@
+export interface User {
+  username: string,
+  password: string,
+  firstname: string,
+  lastname: number,
+  middlename: string,
+  email: string
+}
---
+++ public/app/src/app/dashboard/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Headers, Response  } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { User } from './user.service';
+
+@Injectable()
+export class UserService {
+
+  constructor( private http: HttpClient ) { }
+
+  create( user: User) {
+    return this.http.post<any>('http://localhost:3000/users', user);
+  }
+}
