3d60ec94433a2659aba46bcd0de85d795e48b2b2
earlhansg <earlhansg@gmail.com>
2018-01-30 03:24:50 +0800

    #34 Update User Template

--- public/app/src/app/dashboard/user/user.component.ts
+++ public/app/src/app/dashboard/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-user',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  form: FormGroup;
+
+  username = new FormControl('',  Validators.required);
+  password = new FormControl('',  Validators.required);
+  firstname = new FormControl('',  Validators.required);
+  lastname = new FormControl('',  Validators.required);
+  email = new FormControl('',  Validators.required);
+
+  model: any = {};
+  title: String;
+
+  loaderVisible: boolean = false;
   constructor() { }
 
   ngOnInit() {
+    this.buildForm();
   }
+
+  buildForm(): void {
+    this.form = this.fb.group({
+      'name':   this.name,
+      'address': this.address,
+      'country': this.country,
+      'phone_number': this.phone_number,
+      'website': this.website,
+      'email': this.email
+    });
+  }
+
 
 }
